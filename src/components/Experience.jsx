e27e25fc7b999e0fd195d63ba9b1ff8edd7a0bfa
Muskan Mansoor <muskanmansoor82@gmail.com>
2025-07-21 05:23:13 +0500

    updated portfolio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,14 +6,18 @@ const Experience = () => (
     <h2 className="experience-title">Professional Experience</h2>
 
     <div className="exp-cards-wrapper">
-      {/* 1. React Internship */}
+      
       <div className="exp-card">
         <div className="exp-icon"><FaBriefcase /></div>
         <div className="exp-content">
-          <h3>React Intern – Innovega</h3>
+          <h3>Sales Executive Innovega</h3>
           <p className="exp-duration">Duration: 3 Months</p>
           <p>
-            Developed responsive UI components using React.js, integrated REST APIs, and collaborated with the development team on real-time web projects.
+           Worked as a Sales Executive where I communicated
+with clients via LinkedIn and social media. This role
+helped me understand client needs, build trust, and
+improved my customer dealing skills that now support
+my freelancing career as a developer
           </p>
         </div>
       </div>
@@ -24,7 +28,7 @@ const Experience = () => (
         <div className="exp-content">
           <h3>Team Collaboration at Aptech</h3>
           <p>
-            Participated in internal competitions and events like TechFest and PetBot Exhibition. Worked with teams using Git and collaborated on large-scale websites.
+            Participated in internal competitions and events like TechFest and PetBot Exhibition. Worked with teams using Git and collaborated on large scale websites.
           </p>
         </div>
       </div>
